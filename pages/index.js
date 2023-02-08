1adc77cd004ba5b2fb89eab1a0c64262f1806750
S-P-H-E <sphe.g.personal@gmail.com>
2023-02-08 20:42:56 +0200

    bug fixes

--- pages/index.js
+++ pages/index.js
@@ -26,13 +26,13 @@ export default function Home() {
     <>
       <Head>
         <title>Stoic Speech</title>
-        <meta name="description" content="Stoic Quotes " />
+        <meta name="description" content="Get insightful quotes from the Stoics" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className={`flex justify-center items-center h-screen flex-col ${inter.className}`}>
         {loading ? (
-          <div className='bg-[#ffffff] text-[#000000] p-16 rounded-xl shadow-2xl text-center max-w-lg animate-pulse'>
+          <div className='cursor-progress bg-[#ffffff] text-[#000000] p-16 rounded-xl shadow-2xl text-center max-w-lg animate-pulse'>
             <p className='w-[300px] font-medium text-2xl h-3 break-words rounded-lg bg-gray-200 text-gray-200 m-2'></p>
             <p className='w-[300px] font-medium text-2xl h-3 break-words rounded-lg bg-gray-200 text-gray-200 m-2'></p>
             <p className='w-[300px] font-medium text-2xl h-3 break-words rounded-lg bg-gray-200 text-gray-200 m-2'></p>
@@ -43,7 +43,7 @@ export default function Home() {
             <p className='flex justify-center items-center uppercase font-medium rounded-lg h-3 w-[200px] bg-gray-200 text-gray-200 m-5'></p>
           </div>
         ) : (
-          <div className='bg-[#ffffff] text-[#000000] p-16 rounded-xl shadow-xl text-center max-w-lg'>
+          <div className=' cursor-default bg-[#ffffff] text-[#000000] p-16 rounded-xl shadow-xl text-center max-w-lg'>
             <p className='font-medium text-2xl break-words'>{`"${quote}"`}</p>
             <p className='pt-6 uppercase text-[#999999] font-medium'>{author}</p>
           </div>
