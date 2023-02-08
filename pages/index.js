d28901676fdbba3f02a7a566143885501b74f8f9
S-P-H-E <sphe.g.personal@gmail.com>
2023-02-08 07:17:35 +0200

    loading logic

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   const [quote, setQuote] = useState('')
   const [author, setAuthor] = useState('')
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     async function fetchData() {
@@ -14,10 +15,10 @@ export default function Home() {
       const data = await res.json()
       setQuote(data.body)
       setAuthor(data.author)
+      setLoading(false)
     }
     fetchData()
   }, [])
-
 
   return (
     <>
@@ -28,10 +29,17 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className={`flex justify-center items-center h-screen ${inter.className}`}>
-        <div className=' bg-[#ffffff] text-[#000000] p-16 rounded-xl shadow-2xl text-center max-w-lg'>
-          <p className='font-medium text-2xl break-words'>{`"${quote}"`}</p>
-          <p className='pt-6 uppercase text-[#999999] font-medium'>{author}</p>
-        </div>
+        {loading ? (
+          <div className=' bg-[#ffffff] text-[#000000] p-16 rounded-xl shadow-2xl text-center max-w-lg animate-pulse'>
+            <p className='font-medium text-2xl break-words rounded-lg text-white'>Be tolerant with others and strict with yourself.</p>
+            <p className='pt-6 uppercase font-medium rounded-lg text-white m-1'>Marcus Aurelius</p>
+          </div>
+        ) : (
+          <div className=' bg-[#ffffff] text-[#000000] p-16 rounded-xl shadow-2xl text-center max-w-lg'>
+            <p className='font-medium text-2xl break-words'>{`"${quote}"`}</p>
+            <p className='pt-6 uppercase text-[#999999] font-medium'>{author}</p>
+          </div>
+        )}
       </div>
     </>
   )
